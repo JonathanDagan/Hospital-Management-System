c772403b066aff5069b4c2b73470a30eea186f67
Jonathan Dagan <jonathandagan12@gmail.com>
2021-06-29 00:38:16 +0300

    added employee objects

---
+++ BL/MedicalEmployee.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace BL
+{
+    public class MedicalEmployee: IEmployee
+    {
+        // public int socialId { get; set; }
+        // public string firstName { get; set; }
+        // public string lastName { get; set; }
+        // public Job position { get; set; }
+        // public void logHours()
+        // {
+
+        // }
+        // public double calcMonthsSalary()
+        // {
+
+        // }
+    }
+}
