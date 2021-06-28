8c98cad173e64b09e04deb1b91ae99c740986d48
Jonathan Dagan <jonathandagan12@gmail.com>
2021-06-29 00:53:27 +0300

    DAL WIP

---
+++ DAL/HospitalSerializer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class HospitalSerializer
+    {
+
+    }
+}
