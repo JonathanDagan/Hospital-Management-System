79476f837690b66b25ed92146afa7a445a25c1d5
Jonathan Dagan <jonathandagan12@gmail.com>
2021-06-29 01:16:01 +0300

    renamed WorkHours to WorkShift

--- BL/WorkShift.cs
+++ BL/WorkShift.cs
@@ -3,12 +3,12 @@ using System.Globalization;
 
 namespace BL
 {
-    public class WorkLog
+    public class WorkShift
     {
         public IEmployee employee { get; }
         public DateTime startTime { get; }
         public DateTime endTime { get; }
-        public WorkLog(IEmployee employee, DateTime startTime, DateTime endTime)
+        public WorkShift(IEmployee employee, DateTime startTime, DateTime endTime)
             => (_employee, _startTime, _endTime)
             = (employee, startTime, endTime);
 
