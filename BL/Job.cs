547d436ffb28d7c79d76129dfe48860e0144c51b
Jonathan Dagan <jonathandagan12@gmail.com>
2021-06-29 00:24:00 +0300

    created Job object and ERank enum

---
+++ BL/Job.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BL
+{
+    public class Job
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public double marketRate { get; set; }
+        public HashSet<ERank> ranks { get; set; }
+        public double riskFactor { get; set; }
+
+    }
+}
