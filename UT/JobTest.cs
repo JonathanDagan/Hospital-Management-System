8d7a03a1e7956fd9edcba5c508aeedff9c6276ed
Jonathan <jonathandagan12@gmail.com>
2021-06-29 13:34:31 +0300

    added test for calculating payRate

--- UT/JobTest.cs
+++ UT/JobTest.cs
@@ -1,18 +1,32 @@
 using NUnit.Framework;
+using System.Collections.Generic;
+
+using BL;
+using DAL;
 
 namespace UT
 {
     public class Tests
     {
+        public static IEnumerable<(Job, double)> GetRanksTestCases
+        {
+            get
+            {
+                yield return (new Job("test job","A job for unit tests", 100, new HashSet<ERank>{ERank.senior, ERank.decisionMaker}), 155);
+            }
+        }
         [SetUp]
         public void Setup()
         {
+
         }
 
         [Test]
-        public void Test1()
+        [TestCaseSource("GetRanksTestCases")]
+        public void TestJobsPayRate((Job, double) testCase)
         {
-            Assert.Pass();
+            var (job, expectedPayRate) = testCase;
+            Assert.AreEqual(job.GetHourlyRate(), expectedPayRate);
         }
     }
 }
