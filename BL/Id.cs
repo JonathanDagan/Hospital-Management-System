c4d652df21c44c9268c4e23c7161eab9c4643c9b
Jonathan <jonathandagan12@gmail.com>
2021-06-29 09:07:49 +0300

    not good now, will check how to implement better

--- BL/Id.cs
+++ BL/Id.cs
@@ -2,8 +2,13 @@ using System;
 
 namespace BL
 {
-    public class Id: string
+    public class Id
     {
+        public string id { get; set; }
         // TODO: limit to only actual ids
+        public Id(string id)
+        {
+            this.id = id;
+        }
     }
 }
